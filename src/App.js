87692bd299a82f7eb46e729a52390a1c6ef12788
Ebin reji <163659176+3bin-05@users.noreply.github.com>
2024-09-03 18:52:22 +0530

    in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddVisitor from './components/AddVisitor';
+import Navbar from './components/Navbar';
+import ViewAllVisitors from './components/ViewAllVisitors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AddVisitor />} />
+          <Route path='/navbar' element={<Navbar/>} />
+          <Route path='/viewallvisitors' element={<ViewAllVisitors/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
